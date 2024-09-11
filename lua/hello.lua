5f50e69034440959b01f91c1c9b23759762ac807
drewbeno1 <benodrew12@gmail.com>
2024-09-11 15:55:04 -0400

    picks a random of 3 verses

--- lua/hello.lua
+++ lua/hello.lua
@@ -1,1 +1,17 @@
-print("2 Timothy 1:7 For God did not give us a spirit of fear, but of power, love and self control.")
+math.randomseed(os.time())
+local random_number = math.random(1, 3)
+
+if
+  random_number == 1
+then
+  print("2 Timothy 1:7 For God did not give us a spirit of fear, but of power, love and self control.")
+elseif
+  random_number == 2
+then
+  print("Colossians 3:17 And whatever you do, in word or deed, do everything in the name of the Lord Jesus, giving thanks to God the Father through him.")
+elseif
+  random_number == 3
+then
+  print("Romans 6:23 For the wages of sin is death, but the free gift of God is eternal life through Jesus Christ.")
+end
+
